815bfd70c240d5737fdea1c6d28c258f040d10d4
Cristian <cristian.polo-g@mail.escuelaing.edu.co>
2025-03-18 18:54:58 -0500

    Delete backend directory

--- backend/src/main/java/com/laboratorio/laboratorio_reservas/repositories/LaboratorioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.laboratorio.laboratorio_reservas.repositories;
-
-import com.laboratorio.laboratorio_reservas.models.Laboratorio;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LaboratorioRepository extends MongoRepository<Laboratorio, String> {
-}
